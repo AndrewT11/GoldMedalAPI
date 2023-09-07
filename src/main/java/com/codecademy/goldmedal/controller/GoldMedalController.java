2559fee989c8e95a87ba423889e81de110df8fff
Andrew Tran <tranandrew@gmail.com>
2023-09-07 10:24:32 -0500

    optional omport removed from Controller

--- src/main/java/com/codecademy/goldmedal/controller/GoldMedalController.java
+++ src/main/java/com/codecademy/goldmedal/controller/GoldMedalController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/countries")
